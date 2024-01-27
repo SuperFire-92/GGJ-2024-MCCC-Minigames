b712da62bfb76aba8d5c283c39214d1033e57195
Brad <114950577+goblinman617@users.noreply.github.com>
2024-01-27 13:16:19 -0500

    FollowMouse script
    followMouse script now is usable by any game object you want

---
+++ Minigames/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
+
+public class FollowMouse : MonoBehaviour
+{
+    private int xMax;
+    private int yMax;
+
+    void Start()
+    {
+        xMax = Screen.width;
+        yMax = Screen.height;
+    }
+
+    void Update()
+    {
+        followMouse();
+    }
+    private void followMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        mousePos.x /= xMax;
+        mousePos.y /= yMax;
+
+        mousePos = Camera.main.ViewportToWorldPoint(mousePos);
+        mousePos.z = 0;
+
+        transform.position = mousePos;
+    }
+}
